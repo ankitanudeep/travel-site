3ad5ac6d2664dc26716c8b07beeac4a6a91834af
Ankit <ankitanudeep@gmail.com>
2018-09-09 14:32:36 +0530

    nested, autoprefixer, postcss-simple-vars

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 var gulp = require('gulp');
 var watch = require('gulp-watch');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssvars = require('postcss-simple-vars');
+var nested = require('postcss-nested');
 
 gulp.task('default', function(){
 	console.log("Hooray - you created a gulp class");
@@ -11,7 +15,17 @@ gulp.task('html', function(){
 
 gulp.task('styles', function(){
 	/*move the contents of css file from point styles.css to dest place using pipe*/
-	return gulp.src('./app/assets/styles/styles.css').pipe(gulp.dest('./app/temp/styles'));
+	return gulp.src('./app/assets/styles/styles.css')
+	
+	/*postcss function expects an array*/
+	/*cssvars is helpful to add the value of vars ex:$mainBlue: #2f5572; value was added from styles.css to temp/styles.css*/
+	/*autoprefixer automatically added required prefixes for 'columns'*/
+	/*we can give 2 arguments as well,.pipe(postcss([cssvars, autoprefixer]))*/
+	
+	/*nested is used to create nested classes*/
+	.pipe(postcss([cssvars, nested, autoprefixer]))
+	.pipe(gulp.dest('./app/temp/styles'));
+	
 });
 
 gulp.task('watch', function(){
