70374d555fc69939f12e61bee7ab8370e5d748f7
Ankit <ankitanudeep@gmail.com>
2018-09-11 01:41:34 +0530

    added browser-sync for css files to auto-reload the site without refresh

--- gulpfile.js
+++ gulpfile.js
@@ -27,12 +27,15 @@ gulp.task('styles', function(){
 	/*nested is used to create nested classes*/
 	.pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
 	.pipe(gulp.dest('./app/temp/styles'));
-	
 });
 
 gulp.task('watch', function(){
-	
 	browsersync.init({
+		/*notify: is optional parameter, if we don't want to see the notification after making a change to css file make it false*/
+		/*tunnel: optional parameter, if we wish to get an URL for external devices in same wifi network*/
+		/*tunnel: true will generate random url everytime, otherwise we can use a fixed string*/
+		/*tunnel: 'ankitanudeep',*/
+		notify: false,
 		server: {
 			baseDir: "app"
 		}
@@ -44,6 +47,14 @@ gulp.task('watch', function(){
 	});
 	
 	watch('./app/assets/styles/**/*.css', function(){
-	gulp.start('styles');
+	gulp.start('cssInject');
 	});
+});
+
+/*The dependencies tasks ['styles'] will be first completed and have a chance to generate the temp/styles/styles.css file*/
+/*After the generation of temp/styles/styles.css file, we will pipe in to the browsersync*/
+gulp.task('cssInject', ['styles'], function() {
+	/*whatever available to the pipe, will bring to the browser using stream function*/
+	/*Benefit: browsersync is used to update the css changes without refreshing the whole page*/
+	gulp.src('./app/temp/styles/styles.css').pipe(browsersync.stream());
 });
