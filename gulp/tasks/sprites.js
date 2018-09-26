c6cb5a5cb69cce1984cf1f9111fcf209ecfb9d3d
Ankit <ankitanudeep@gmail.com>
2018-09-27 01:36:17 +0530

    sprite adjustments

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp');
 var svgSprite = require('gulp-svg-sprite');
+var rename = require('gulp-rename');
 
 var config = {
 	mode: {
 		css: {
+			sprite: 'svg/sprite.svg',
 			render: {
 				css:{
 					template: './gulp/templates/sprite.css'
@@ -18,3 +20,20 @@ gulp.task('createSprite', function() {
 		.pipe(svgSprite(config))
 		.pipe(gulp.dest('./app/temp/sprite/'))
 });
+
+gulp.task('copySpriteGraphic', ['createSprite'], function() {
+	return gulp.src('./app/temp/sprite/css/**/*.svg')
+	.pipe(gulp.dest('./app/assets/images/sprites'));
+});
+
+/*this task will have dependency on the createSprite task -- synchronized*/
+gulp.task('copySpriteCSS', ['createSprite'], function() {
+	return gulp.src('./app/temp/sprite/css/*.css')
+	.pipe(rename('_sprite.css'))
+	.pipe(gulp.dest('./app/assets/styles/modules'))
+});
+
+/*both these tasks will start together, sometimes it may cause inconsistency*
+Hence we have added dependency in the second task copySpriteCSS to be dependent on
+the first task createSprite to be completed*/
+gulp.task('icons', ['createSprite', 'copySpriteGraphic', 'copySpriteCSS']);
