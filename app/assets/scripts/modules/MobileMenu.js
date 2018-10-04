961d884cb55197ef166a19071afe170c1666478f
Ankit <ankitanudeep@gmail.com>
2018-10-04 15:01:52 +0530

    finished hamburger shape

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -15,6 +15,7 @@ class MobileMenu {
     toggletheMenu() {
         this.menuContent.toggleClass("site-header__menu-content--is-visible");
         this.siteheader.toggleClass("site-header--is-expanded");
+        this.menuicon.toggleClass("site-header__menu-icon-close-x");
     }
 }
 
